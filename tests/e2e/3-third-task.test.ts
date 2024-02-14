1cf9a39aa78db84c83fd9c28b387cbcce7d305bb
Ny Hasinavalona Randriantsarafara <nyhasinavalonar@gmail.com>
2024-02-14 14:58:15 +0100

    chore: added ThirdPartyApp1 and ThirdPartyApp2 constants

--- tests/e2e/3-third-task.test.ts
+++ tests/e2e/3-third-task.test.ts
@@ -3,6 +3,7 @@ const request = require('supertest');
 import app from '../../src/app';
 import { AppOneTicketStatus } from '../../src/object-values/app-one-ticket.status';
 import { AppTwoTicketStatus } from '../../src/object-values/app-two-ticket.status';
+import { ThirdPartyApp } from '../../src/object-values/third-party.app';
 import { ProblemStatus } from '../../src/object-values/problem.status';
 
 describe('Task 3', () => {
@@ -42,12 +43,14 @@ describe('Task 3', () => {
         },
       ]);
     await request(app).post(`/change-status/${ProblemStatus.READY}`).send();
-    const { status, body: ticket } = await request(app).post(`/create-ticket/ThirdPartyApp1/${problem.id}`).send({
-      problemId: problem.id,
-      myStatus: AppOneTicketStatus.RUNNING,
-      count: 1,
-      owner: 'csTeam',
-    });
+    const { status, body: ticket } = await request(app)
+      .post(`/create-ticket/${ThirdPartyApp.APP_ONE}/${problem.id}`)
+      .send({
+        problemId: problem.id,
+        myStatus: AppOneTicketStatus.RUNNING,
+        count: 1,
+        owner: 'csTeam',
+      });
     expect(status).toBe(201);
     expect(ticket).toEqual({
       problemId: problem.id,
@@ -71,12 +74,14 @@ describe('Task 3', () => {
         },
       ]);
     await request(app).post(`/change-status/${ProblemStatus.READY}`).send();
-    const { status, body: ticket } = await request(app).post(`/create-ticket/ThirdPartyApp2/${problem.id}`).send({
-      problemRef: problem.id,
-      status: AppTwoTicketStatus.ON_GOING,
-      issuesCount: 1,
-      externalOwner: 'csTeam',
-    });
+    const { status, body: ticket } = await request(app)
+      .post(`/create-ticket/${ThirdPartyApp.APP_TWO}/${problem.id}`)
+      .send({
+        problemRef: problem.id,
+        status: AppTwoTicketStatus.ON_GOING,
+        issuesCount: 1,
+        externalOwner: 'csTeam',
+      });
     expect(status).toBe(201);
     expect(ticket).toEqual({
       problemRef: problem.id,
