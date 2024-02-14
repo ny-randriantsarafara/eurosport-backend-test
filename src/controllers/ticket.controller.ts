6161c59d2331a9170ec4e023eac071d1baa7d1ae
Ny Hasinavalona Randriantsarafara <nyhasinavalonar@gmail.com>
2024-02-14 15:24:56 +0100

    chore: implemented update ticket controller

--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { ThirdPartyApp } from '../object-values/third-party.app';
 import ProblemService from '../services/problem.service';
 import ThirdPartyAppService from '../services/third-party-app.service';
+import { UpdateOrCloseTicketDto } from '../dtos/update-or-close-ticket.dto';
 
 export const createTicketController = (req: Request, res: Response) => {
   const {
@@ -26,5 +27,29 @@ export const createTicketController = (req: Request, res: Response) => {
     res.status(201).send(createdTicket);
   } catch (error: any) {
     res.status(400).send({ message: error?.message });
+  }
+};
+
+export const updateTicketController = (req: Request, res: Response) => {
+  const {
+    params: { thirdPartyApp, problemId },
+  } = req;
+  const { body: args } = req;
+  const thirdPartyAppService = ThirdPartyAppService.getInstance();
+  if (!thirdPartyAppService.isValidThirdPartyApps(thirdPartyApp)) {
+    res.status(400).send({ message: 'Invalid third party app' });
+    return;
+  }
+  const updateTicketDto: UpdateOrCloseTicketDto = {
+    appName: thirdPartyApp,
+    count: thirdPartyApp === ThirdPartyApp.APP_ONE ? args.count : args.issuesCount,
+  };
+  try {
+    const problemService = ProblemService.getInstance();
+    const problem = problemService.getProblemById(problemId);
+    const updatedTicket = thirdPartyAppService.updateTicket(updateTicketDto, problem);
+    res.status(201).send(updatedTicket);
+  } catch (e: any) {
+    res.status(400).send({ message: e?.message });
   }
 };
