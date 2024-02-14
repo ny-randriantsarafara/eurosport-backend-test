ab01f20e65c2ea83f802b179601b75e167eee7e1
Ny Hasinavalona Randriantsarafara <nyhasinavalonar@gmail.com>
2024-02-14 14:32:18 +0100

    feat: create issue controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import { createIssuesController } from './controllers/issue.controller';
 
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
+
+app.post('/issues', createIssuesController);
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
